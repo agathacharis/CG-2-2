064a3dd80a728eb25a84cf1c8b4a8a37aac0d6b2
Agatha Berwick <agatha.berwick@gmail.com>
2018-07-19 17:55:08 -0500

    space added

--- CG 2-2/Program.cs
+++ CG 2-2/Program.cs
@@ -6,7 +6,14 @@ namespace CG_2_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int area, length, width;
+            Console.Write("Please write the length of your rectangle:");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please write the width of your rectanlge: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
+            Console.WriteLine("The area of rectangle : {0}", area);
+            Console.ReadKey();
         }
     }
 }
